a0b802b8eb2446d6234441b89a25710b05340358
manjula <manjula.devarala@cesneosilica.com>
2024-04-23 16:19:34 +0530

    new changes

--- src/UseEffectsimilar.jsx
+++ src/UseEffectsimilar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 function UseEffectSimilar() {
     const [count, setCount] = useState(0); // State to keep track of count
-    const [input, setInput] = useState(''); // State to keep track of text input
+    const [input1, setInput1] = useState(''); // State to keep track of text input
+    const [input2, setInput2] = useState(''); // State to keep track of text input
 
     // Function to increase count
     const increase = () => {
@@ -15,8 +16,15 @@ function UseEffectSimilar() {
     };
 
     // Function to update input state based on input field changes
-    const handleInput = (e) => {
-        setInput(e.target.value);
+    const handleInput1 = (e) => {
+        setInput1(e.target.value);
+        setInput2(e.target.value);
+    };
+
+     // Function to update input state based on input field changes
+     const handleInput2 = (e) => {
+        setInput2(e.target.value);
+        setInput1(e.target.value);
     };
 
     return (
@@ -26,11 +34,8 @@ function UseEffectSimilar() {
         <h3>With input field ( mirror Effect)</h3>
             <h4>{count}</h4>
             {/* Input field for entering text */}
-            <input type="text" className='w-50 d-flex justify-content-center' value={input} onChange={handleInput} /><br />
-            {/* Display the entered text */}
-            {/* <p>{input}</p> */}
-            {/* Second input that mirrors the text entered in the first input */}
-            <input type="text" className='w-50 d-flex justify-content-center' value={input} onChange={handleInput} /><br />
+            <input type="text" className='w-50 d-flex justify-content-center' value={input1} onChange={handleInput1} /><br />
+            <input type="text" className='w-50 d-flex justify-content-center' value={input2} onChange={handleInput2}/><br />
           <div className='d-flex justify-content-center mb-3'>
           <button onClick={increase} className='mt-3 me-3 btn btn-primary'>Increase</button>
             <button onClick={decrease} className='mt-3 btn btn-primary'>Decrease</button>
