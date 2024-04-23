4e992e1b49c67d584c65cd6a88f8c3e8d768d92f
manjula <manjula.devarala@cesneosilica.com>
2024-04-23 14:28:37 +0530

    login page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <App />
-    {/* <Header /> */}
   </React.StrictMode>
 );
 
